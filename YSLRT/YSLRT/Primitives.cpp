0f5d7e2fb45f5edd766389dcf4d4ed5a7ebf5f81
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2016-05-15 02:48:49 +0300

    sphere intersect

--- YSLRT/YSLRT/Primitives.cpp
+++ YSLRT/YSLRT/Primitives.cpp
@@ -6,7 +6,28 @@ using namespace glm;
 
 float Sphere::intersect(Ray ray)
 {
-	return 0.0f;
+	float x0, x1;
+	vec3 dist = ray.origin - c;
+
+	// Quadratic equation
+	float a = dot(ray.direction, ray.direction);
+	float b = 2 * dot(ray.direction, dist);
+	float c = dot(dist, dist) - (r * r);
+	float discr = b * b - 4 * a * c;
+
+	if (discr < 0) return -1.0f;
+	else if (discr == 0) x0 = x1 = -0.5 * b / a;
+	else {
+		float q = (b > 0) ?
+			-0.5 * (b + sqrt(discr)) :
+			-0.5 * (b - sqrt(discr));
+		x0 = q / a;
+		x1 = c / q;
+	}
+
+	if (!(x0 < 0) && x0 < x1)
+		return x0;
+	return x1;
 }
 
 /* Copypaste on paras
--- YSLRT/YSLRT/main.cpp
+++ YSLRT/YSLRT/main.cpp
@@ -8,7 +8,9 @@ int main(void)
 {
 	std::vector<Primitive*> map;
 	Triangle tri(glm::vec3(0.0f,0.5f,1.0f), glm::vec3(0.5f,0.0f,1.0f), glm::vec3(-0.5,0.0f,1.0f));
+	Sphere sph(glm::vec3(0.0f, 0.5f, 0.5f), 0.2f);
 	map.push_back(&tri);
+	map.push_back(&sph);
 
 	Raytracer rt(1980, 1080);
 	rt.setMap(&map);
