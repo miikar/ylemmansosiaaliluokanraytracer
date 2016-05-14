bc4f5b659641769225e78888f3d52c021742e297
Aarnio Lauri <git@git.niksula.hut.fi:aarniol1>
2016-05-14 15:38:36 +0300

    Vähän pohjaa raytracing loopille

--- YSLRT/YSLRT/Raytracer.cpp
+++ YSLRT/YSLRT/Raytracer.cpp
@@ -3,6 +3,28 @@
 #include <glm/glm.hpp>
 #include "Raytracer.hpp"
 
+void Raytracer::calculatePixels(void){
+	for (int x = 0; x < width; x++){
+		for (int y = 0; y < height; y++){
+			using namespace glm;
+			static vec2 resolution = vec2((float)width, (float)height);
+
+			/* RAYTRACING STARTS HERE */
+
+			vec2 pIndex = vec2((float)x, (float)y);							  /* Current pixel (x,y) */
+			vec2 uv = vec2(pIndex.x / resolution.x, pIndex.x / resolution.y); /* Pixel coordinates uv [0.0,1.0] */
+			vec2 p = uv*2.0f - 1.0f;										  /* Pixel coordinates p [-1.0,1.0] */
+			p.x *= resolution.x / resolution.y;								  /* Adjust p to aspect ratio */
+
+			vec3 color = vec3(1.0f*p.y);
+			
+
+			clamp(color, 0.0f, 1.0f);
+			pixelData[x+width*y] = color;
+		}
+	}
+}
+
 void Raytracer::dataToFile(std::string path) {
 	// BMPFILEHEADER
 	// BMPINFOHEADER
@@ -13,7 +35,4 @@ void Raytracer::dataToFile(std::string path) {
 	FILE *f;
 	f = fopen("img.bmp", "w");
 	fclose(f);
-	Ray ray(glm::vec3(1.0f), glm::vec3(1.0f));
-	ray.direction;
-	ray.origin;
 }
