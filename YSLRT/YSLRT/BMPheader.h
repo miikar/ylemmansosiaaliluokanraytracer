139f2492d1e94967343bf236832011df5540024c
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2016-05-15 01:15:04 +0300

    bmp types

--- YSLRT/YSLRT/BMPheader.h
+++ YSLRT/YSLRT/BMPheader.h
@@ -1,29 +1,31 @@
+#include <stdint.h>
+
 #pragma pack(push)
-#pragma pack(1) 
+#pragma pack(1)
 
 struct BMPFILEHEADER
 {
-	__int16 bfType = 19778;
-	__int32 bfSize;
-	__int16 bfReserved1 = 0;
-	__int16 bfReserved2 = 0;
-	__int32 bfOffBits = 54;
+	uint16_t bfType = 19778;
+	uint32_t bfSize;
+	uint16_t bfReserved1 = 0;
+	uint16_t bfReserved2 = 0;
+	uint32_t bfOffBits = 54;
 }bmfh;
 
 
 struct BMPINFOHEADER
 {
-	__int32 biSize = 40;
-	__int32 biWidth;
-	__int32 biHeight;
-	__int16 biPlanes = 1;
-	__int16 biBitCount = 24;
-	__int32 biCompression = 0;
-	__int32 biSizeImage;
-	__int32 biXPelsPerMeter;
-	__int32 biYPelsPerMeter;
-	__int32 biClrUsed = 0;
-	__int32 biClrImportant = 0;
+	uint32_t biSize = 40;
+	uint32_t biWidth;
+	uint32_t biHeight;
+	uint16_t biPlanes = 1;
+	uint16_t biBitCount = 24;
+	uint32_t biCompression = 0;
+	uint32_t biSizeImage;
+	uint32_t biXPelsPerMeter;
+	uint32_t biYPelsPerMeter;
+	uint32_t biClrUsed = 0;
+	uint32_t biClrImportant = 0;
 }bmih;
 
 #pragma pack(pop) 
