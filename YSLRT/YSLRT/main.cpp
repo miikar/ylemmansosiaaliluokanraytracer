5a6c6c85d924faec6c8a73bbe17a8aa4a99fd399
Aarnio Lauri <git@git.niksula.hut.fi:aarniol1>
2016-05-15 16:27:54 +0300

    Color ominaisuus lisätty

--- YSLRT/YSLRT/main.cpp
+++ YSLRT/YSLRT/main.cpp
@@ -18,8 +18,8 @@ int main(void)
 
 
 	std::vector<Primitive*> map;
-	Triangle tri(glm::vec3(0.0f,0.5f,1.0f), glm::vec3(0.5f,0.0f,1.0f), glm::vec3(-0.5,0.0f,1.0f));
-	Sphere sph(glm::vec3(0.0f, 0.5f, 0.5f), 0.2f);
+	Triangle tri(glm::vec3(0.0f,0.5f,1.0f), glm::vec3(0.5f,0.0f,1.0f), glm::vec3(-0.5,0.0f,1.0f), glm::vec3(0.3f,0.4f,0.7f));
+	Sphere sph(glm::vec3(0.0f, 0.5f, 0.5f), 0.2f, glm::vec3(0.6f,0.4f,0.3f));
 	map.push_back(&tri);
 	map.push_back(&sph);
 
