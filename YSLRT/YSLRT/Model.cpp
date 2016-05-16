295793db8c85655e65a25b3b725f93ef79b4621b
Aarnio Lauri <git@git.niksula.hut.fi:aarniol1>
2016-05-16 21:54:27 +0300

    fscanf_s

--- YSLRT/YSLRT/Model.cpp
+++ YSLRT/YSLRT/Model.cpp
@@ -23,19 +23,19 @@ void Model::OBJparse(string path) {
 		fprintf(stderr, "erno %d apuva!\n", erno);
 
 	while (1) {
-		int ret = fscanf(obj_file, "%s", token);
+		int ret = fscanf_s(obj_file, "%s", token);
 		if (ret == EOF)
 			break;
 
 		if ( strcmp(token, "v") == 0) {
 			glm::vec3 vertex;
-			fscanf(obj_file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			fscanf_s(obj_file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
 			vertices.push_back(vertex);
 		} else if ( strcmp(token, "f") == 0) {
 			// vertex index/texture coordinate/normal
 			// Currently scans only indexes
 			unsigned int vertexIndex[3];
-			fscanf(obj_file, "%d %d %d", &vertexIndex[0], &vertexIndex[1], &vertexIndex[2]);
+			fscanf_s(obj_file, "%d %d %d", &vertexIndex[0], &vertexIndex[1], &vertexIndex[2]);
 			vertexIndices.push_back(vertexIndex[0]);
 			vertexIndices.push_back(vertexIndex[1]);
 			vertexIndices.push_back(vertexIndex[2]);
